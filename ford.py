91638d70ad43905f19e2066fbbbb306896de6a16
Argisht1111 <156825469+Argisht1111@users.noreply.github.com>
2024-03-09 23:18:17 +0300

    Add files via upload

---
+++ ford.py
@@ -0,0 +1,78 @@
+
+#Задание1
+# class Car():
+#     def __init__(self):
+#         self.model = ""
+#         self.date = 0
+#         self.capacity = 0.0
+#         self.color = ""
+#         self.price = 0.0
+#     def input_date(self):
+#         self.model = input('Введите модель автомобилья ')
+#         self.date = input('Введите год выпуска автомобиля ')
+#         self.capacity = input('Введите объем двигателя ')
+#         self.color = input('Введите цвет автомобиля ')
+#         self.price = input('Введите цену автомобиля ')
+#     def gela(self):
+#         print(f'модель {self.model} {self.date} года выпуска с объемом двигателя {self.capacity}литра {self.color} цвета и стоимости {self.price} рублей ')
+#     def get_model(self):
+#         print(self.model)
+#         print(self.date)
+#         print(self.capacity)
+#         print(self.color)
+#         print(self.price)
+#
+# x = Car()
+# x.input_date()
+# x.gela()
+# x.get_model()
+
+# Задание 2
+# class book():
+#     def __init__(self):
+#         self.name = ""
+#         self.date = 0
+#         self.capacity = ""
+#         self.genre = ""
+#         self.author = ""
+#         self.price = 0.0
+#     def input_(self):
+#         self.name = input('Введите название книги: ')
+#         self.date = input('Введите год выпуска книги: ')
+#         self.capacity = input('Введите производителя книги: ')
+#         self.genre = input('Введите жанр книги: ')
+#         self.author = input('Введите автора книги: ')
+#         self.price = input('Введите цену книги: ')
+#     def output_(self):
+#         print(f'Название книги: {self.name}')
+#         print(f'год выпуска: {self.date}')
+#         print(f'производитель книги: {self.capacity}')
+#         print(f'Жанр книги: {self.genre}')
+#         print(f'Автор книги: {self.author}')
+#         print(f'Цена книги: {self.price}')
+#
+# a = book()
+# a.input_()
+# a.output_()
+# Задание 3
+# class stadion():
+#     def __init__(self):
+#         self.name = ""
+#         self.date = 0
+#         self.country = ""
+#         self.city = ""
+#         self.capacity = ""
+#     def input_(self):
+#         self.name = input('Введите название стадиона: ')
+#         self.date = input('Введите дату открытия стадиона: ')
+#         self.country = input('Введите страну: ')
+#         self.city = input('Введите город: ')
+#         self.capacity = input('Введите вместимость: ')
+#     def output_(self):
+#         print("название стадиона", self.name, "дата открытия стадиона", self.date, "страна", self.country, "город", self.city, "вместимость", self.capacity)
+#
+# a = stadion()
+# a.input_()
+# a.output_()
+
+
